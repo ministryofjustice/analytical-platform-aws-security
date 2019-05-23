c24b8eeb14c342aa71413e46126e61812aacc6d9
Oliver Butterbach <oliver.butterbach@digital.justice.gov.uk>
2019-05-23 11:48:52 +0100

    Remove unnecessary role from guardduty module

--- modules/guardduty/main.tf
+++ modules/guardduty/main.tf
@@ -8,50 +8,6 @@ resource "aws_guardduty_detector" "guardduty" {
 }
 
 # -----------------------------------------------------------
-# set up iam group with appropriate iam policy
-# -----------------------------------------------------------
-
-resource "aws_iam_policy" "enable_guardduty" {
-  name        = "enable-guardduty"
-  description = "Allows setup and configuration of GuardDuty"
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "guardduty:*"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "iam:CreateServiceLinkedRole"
-            ],
-            "Resource": "arn:aws:iam::${var.landing_account_id}:role/aws-service-role/guardduty.amazonaws.com/AWSServiceRoleForAmazonGuardDuty",
-            "Condition": {
-                "StringLike": {
-                    "iam:AWSServiceName": "guardduty.amazonaws.com"
-                }
-            }
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "iam:PutRolePolicy",
-                "iam:DeleteRolePolicy"
-            ],
-            "Resource": "arn:aws:iam::${var.landing_account_id}:role/aws-service-role/guardduty.amazonaws.com/AWSServiceRoleForAmazonGuardDuty"
-        }
-    ]
-}
-EOF
-}
-
-# -----------------------------------------------------------
 # set up AWS Cloudwatch Event rule for Guardduty Findings
 # also to set up an event pattern (json file)
 # -----------------------------------------------------------
