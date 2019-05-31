c108586df182f154bc1101766ace84c6de0949a8
Oli <4367158+obutterbach@users.noreply.github.com>
2019-05-31 14:03:10 +0100

    Link prod / data guardduty to landing guardduty account (#10)

--- main.tf
+++ main.tf
@@ -11,6 +11,22 @@ module "aws_guardduty_invite_dev" {
   assume_role_in_account_id = "${var.ap_accounts["landing"]}"
 }
 
+module "aws_guardduty_invite_prod" {
+  source                    = "modules/guardduty-invitation"
+  detector_master_id        = "${module.aws_guardduty_master.guardduty_master_id}"
+  email_member_parameter    = "${var.email_member_parameter_prod}"
+  member_account_id         = "${var.ap_accounts["prod"]}"
+  assume_role_in_account_id = "${var.ap_accounts["landing"]}"
+}
+
+module "aws_guardduty_invite_data" {
+  source                    = "modules/guardduty-invitation"
+  detector_master_id        = "${module.aws_guardduty_master.guardduty_master_id}"
+  email_member_parameter    = "${var.email_member_parameter_data}"
+  member_account_id         = "${var.ap_accounts["data"]}"
+  assume_role_in_account_id = "${var.ap_accounts["landing"]}"
+}
+
 module "aws_guardduty_member_dev" {
   source                    = "modules/guardduty-member"
   master_account_id         = "${var.ap_accounts["landing"]}"
