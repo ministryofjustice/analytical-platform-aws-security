dfa9329948c78a673c0cd4e004fe41d0adfdaea1
Oliver Butterbach <oliver.butterbach@digital.justice.gov.uk>
2019-06-05 09:16:55 +0100

    Pass missing variables to sns integration module

--- main.tf
+++ main.tf
@@ -4,9 +4,11 @@ module "aws_guardduty_master" {
 }
 
 module "aws_guardduty_sns_notifications" {
-  source                    = "modules/sns-slack-integration"
-  event_rule                = "${module.aws_guardduty_master.guardduty_event_rule}"
-  assume_role_in_account_id = "${var.ap_accounts["landing"]}"
+  source                     = "modules/sns-slack-integration"
+  event_rule                 = "${module.aws_guardduty_master.guardduty_event_rule}"
+  ssm_slack_channel          = "${var.ssm_slack_channel}"
+  ssm_slack_incoming_webhook = "${var.ssm_slack_incoming_webhook}"
+  assume_role_in_account_id  = "${var.ap_accounts["landing"]}"
 }
 
 module "aws_guardduty_invite_dev" {
