f998b0c75ed44a5eb1187d02dd5845a27ed7025c
Oli <4367158+obutterbach@users.noreply.github.com>
2019-06-07 10:11:21 +0100

    Add proper right for cloudwatch event to invoke sns (#26)

--- modules/sns-slack-integration/lambda.tf
+++ modules/sns-slack-integration/lambda.tf
@@ -22,6 +22,31 @@ EOF
 }
 
 # -----------------------------------------------------------
+# Create policy for CloudWatch Event - SNS
+# -----------------------------------------------------------
+
+data "aws_iam_policy_document" "sns_topic_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["SNS:Publish"]
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+    resources = ["${aws_sns_topic.guardduty_sns.arn}"]
+  }
+}
+
+# -----------------------------------------------------------
+# Attach Policy to SNS
+# -----------------------------------------------------------
+
+resource "aws_sns_topic_policy" "default" {
+  arn    = "${aws_sns_topic.guardduty_sns.arn}"
+  policy = "${data.aws_iam_policy_document.sns_topic_policy.json}"
+}
+
+# -----------------------------------------------------------
 # Create policy for logging
 # -----------------------------------------------------------
 
