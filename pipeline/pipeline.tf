7e84d7c3e23c17201192742a38eb7f07c0021b29
Oli <4367158+obutterbach@users.noreply.github.com>
2019-06-10 10:11:11 +0100

    Rename guardduty repo into aws security (#32)

--- pipeline/pipeline.tf
+++ pipeline/pipeline.tf
@@ -9,8 +9,8 @@ data "aws_iam_policy_document" "codebuild_policy" {
 module "pipeline" {
   source = "github.com/ministryofjustice/analytical-platform-pipeline"
 
-  name = "guardduty-pipeline"
-  pipeline_github_repo = "analytical-platform-guardduty"
+  name = "aws-security"
+  pipeline_github_repo = "analytical-platform-aws-security"
   pipeline_github_owner = "ministryofjustice"
   pipeline_github_branch = "master"
   codebuild_policy = "${data.aws_iam_policy_document.codebuild_policy.json}"
