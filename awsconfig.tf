0db4da1e35551919084834519e052b2a84a21595
Oli <4367158+obutterbach@users.noreply.github.com>
2019-06-11 14:06:14 +0100

    Add aggregated architecture (#38)

--- awsconfig.tf
+++ awsconfig.tf
@@ -1,5 +1,17 @@
 # AWS Config baseline
 module "aws-config" {
-  source = "modules/aws-config"
-  assume_role_in_account_id = "${var.ap_accounts["landing"]}"
+  source                     = "modules/aws-config-main"
+  assume_role_in_account_id  = "${var.ap_accounts["landing"]}"
+}
+
+module "aws-config-aggregated-dev" {
+  source                     = "modules/aws-config-aggregated"
+  main_aws_config_account_id = "${var.ap_accounts["landing"]}"
+  assume_role_in_account_id  = "${var.ap_accounts["dev"]}"
+}
+
+module "aws-config-authorization-dev" {
+  source                     = "modules/aws-config-authorized"
+  aggregated_account_id      = "${var.ap_accounts["dev"]}"
+  assume_role_in_account_id  = "${var.ap_accounts["landing"]}"
 }
---
+++ modules/aws-config-aggregated/main.tf
@@ -0,0 +1,149 @@
+# -----------------------------------------------------------
+# AWS Config Recorder
+# -----------------------------------------------------------
+
+resource "aws_config_configuration_recorder" "recorder" {
+  name                = "aws-config-recorder"
+  role_arn            = "${aws_iam_role.config.arn}"
+}
+
+# -----------------------------------------------------------
+# AWS Config Role Recorder
+# -----------------------------------------------------------
+
+resource "aws_iam_role" "config" {
+  name = "config-service"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "config.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
+}
+
+# -----------------------------------------------------------
+# AWS Config Delivery Channel
+# -----------------------------------------------------------
+
+resource "aws_config_delivery_channel" "delivery_channel" {
+  name                = "aws-config-delivery-channel"
+  s3_bucket_name      = "${aws_s3_bucket.awsconfigbucket.bucket}"
+  depends_on          = ["aws_config_configuration_recorder.recorder"]
+}
+
+resource "aws_config_configuration_recorder_status" "recorder_status" {
+  name                = "${aws_config_configuration_recorder.recorder.name}"
+  is_enabled          = true
+  depends_on          = ["aws_config_delivery_channel.delivery_channel"]
+}
+
+# -----------------------------------------------------------
+# Bucket for AWS Config
+# -----------------------------------------------------------
+
+resource "aws_s3_bucket" "awsconfigbucket" {
+  bucket              = "aws-config-s3bucket-${var.assume_role_in_account_id}"
+  acl                 = "private"
+  versioning {
+    enabled = true
+  }
+  lifecycle {
+    prevent_destroy = false
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+# -----------------------------------------------------------
+# Bucket Policy for AWS Config
+# -----------------------------------------------------------
+
+resource "aws_iam_policy" "s3_config_policy" {
+  name = "awsconfig-bucket-access"
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "${aws_s3_bucket.awsconfigbucket.arn}",
+        "${aws_s3_bucket.awsconfigbucket.arn}/*"
+      ]
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "aws_config" {
+  role                = "${aws_iam_role.config.name}"
+  policy_arn          = "arn:aws:iam::aws:policy/service-role/AWSConfigRole"
+}
+
+resource "aws_iam_role_policy_attachment" "config_s3_policy" {
+  role                = "${aws_iam_role.config.name}"
+  policy_arn          = "${aws_iam_policy.s3_config_policy.arn}"
+}
+
+# -----------------------------------------------------------
+# AWS Config Rules
+# -----------------------------------------------------------
+
+resource "aws_config_configuration_aggregator" "account" {
+  name = "landing-aws-config"
+  account_aggregation_source {
+    account_ids = ["${var.main_aws_config_account_id}"]
+    regions     = ["${var.region}"]
+  }
+}
+
+
+# -----------------------------------------------------------
+# AWS Config Rules
+# -----------------------------------------------------------
+
+resource "aws_config_config_rule" "root_account_mfa_enabled" {
+  name                = "root_account_mfa_enabled"
+  source {
+    owner             = "AWS"
+    source_identifier = "ROOT_ACCOUNT_MFA_ENABLED"
+  }
+  depends_on          = ["aws_config_configuration_recorder.recorder"]
+}
+
+resource "aws_config_config_rule" "cloud_trail_enabled" {
+  name                = "cloud_trail_enabled"
+  source {
+    owner             = "AWS"
+    source_identifier = "CLOUD_TRAIL_ENABLED"
+  }
+  depends_on          = ["aws_config_configuration_recorder.recorder"]
+}
+
+resource "aws_config_config_rule" "cloud-trail-cloud-watch-logs-enabled" {
+  name                = "cloud-trail-cloud-watch-logs-enabled"
+  source {
+    owner             = "AWS"
+    source_identifier = "CLOUD_TRAIL_CLOUD_WATCH_LOGS_ENABLED"
+  }
+  depends_on          = ["aws_config_configuration_recorder.recorder"]
+}
