597ee77be02c2fdfe32e6a48848e865086331f44
Oliver Butterbach <oliver.butterbach@digital.justice.gov.uk>
2019-06-24 10:09:59 +0100

    indicate list to var

--- modules/lambda-unused-credentials/lambda.tf
+++ modules/lambda-unused-credentials/lambda.tf
@@ -124,6 +124,6 @@ data "template_file" "cloudformation_sns_stack" {
   template = "${file("${path.module}/email-sns-stack.json.tpl")}"
   vars {
     display_name  = "${var.display_name}"
-    subscriptions = "${join("," , formatlist("{ \"Endpoint\": \"%s\", \"Protocol\": \"%s\" }", data.aws_ssm_parameter.unused_credentials_emails.value, var.protocol))}"
+    subscriptions = "${join("," , formatlist("{ \"Endpoint\": \"%s\", \"Protocol\": \"%s\" }", list(data.aws_ssm_parameter.unused_credentials_emails.value), var.protocol))}"
   }
 }
