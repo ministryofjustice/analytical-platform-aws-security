f8b1231da840fe2c75f60b2037f47a66ab3832d5
Oli <4367158+obutterbach@users.noreply.github.com>
2019-06-24 09:09:08 +0100

    Ob/fix path (#50)
    * Fix path to template folder

--- modules/lambda-unused-credentials/lambda.tf
+++ modules/lambda-unused-credentials/lambda.tf
@@ -123,6 +123,6 @@ data "template_file" "cloudformation_sns_stack" {
   template = "${file("${path.module}/email-sns-stack.json.tpl")}"
   vars {
     display_name  = "${var.display_name}"
-    subscriptions = "${join("," , formatlist("{ \"Endpoint\": \"%s\", \"Protocol\": \"%s\" }", data.aws_ssm_parameter.unused_credentials_email.value, var.protocol))}"
+    subscriptions = "${join("," , formatlist("{ \"Endpoint\": \"%s\", \"Protocol\": \"%s\" }", data.aws_ssm_parameter.unused_credentials_emails.value, var.protocol))}"
   }
 }
