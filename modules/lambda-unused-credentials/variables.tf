bcffefeeded3b13a0ba915c3928a15c15a6e5128
Oli <4367158+obutterbach@users.noreply.github.com>
2019-06-24 08:41:13 +0100

    Add missing role for scanning unused credentials (#48)

--- modules/lambda-unused-credentials/variables.tf
+++ modules/lambda-unused-credentials/variables.tf
@@ -25,6 +25,10 @@ variable "lambda_function_name" {
   default = "lambda-unused-credentials"
 }
 
+variable "lambda_unused_credentials_role" {
+  default = "lambda-unused-credentials-role"
+}
+
 variable "protocol" {
   default     = "email"
   description = "SNS Protocol to use. email or email-json"
