cfe949d24734afc37ee5a45896891f479438206e
Oli <4367158+obutterbach@users.noreply.github.com>
2019-06-24 09:05:17 +0100

    Fix path to template folder (#49)

--- modules/lambda-unused-credentials/lambda.tf
+++ modules/lambda-unused-credentials/lambda.tf
@@ -120,7 +120,7 @@ resource "aws_cloudformation_stack" "sns_topic" {
 # -----------------------------------------------------------
 
 data "template_file" "cloudformation_sns_stack" {
-  template = "${file("${path.module}/templates/email-sns-stack.json.tpl")}"
+  template = "${file("${path.module}/email-sns-stack.json.tpl")}"
   vars {
     display_name  = "${var.display_name}"
     subscriptions = "${join("," , formatlist("{ \"Endpoint\": \"%s\", \"Protocol\": \"%s\" }", data.aws_ssm_parameter.unused_credentials_email.value, var.protocol))}"
