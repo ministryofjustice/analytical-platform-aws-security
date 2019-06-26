ac8fc5ce6f4b56106cae0215902a59a66467e1e8
Oli <4367158+obutterbach@users.noreply.github.com>
2019-06-26 12:05:13 +0100

    Rename modules (#75)

--- unused_credentials.tf
+++ unused_credentials.tf
@@ -5,13 +5,13 @@ module "unused-credentials" {
 }
 
 # Scan for unused credentials with lambda function dev account
-module "unused-credentials" {
+module "unused-credentials-dev" {
   source                     = "modules/lambda-unused-credentials"
   assume_role_in_account_id  = "${var.ap_accounts["dev"]}"
 }
 
 # Scan for unused credentials with lambda function prod account
-module "unused-credentials" {
+module "unused-credentials-prod" {
   source                     = "modules/lambda-unused-credentials"
   assume_role_in_account_id  = "${var.ap_accounts["prod"]}"
 }
