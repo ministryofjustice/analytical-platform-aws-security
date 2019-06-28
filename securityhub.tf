1d6580a08cf1f8beeb6c009a8b08f6946c6620d1
Oliver Butterbach <oliver.butterbach@digital.justice.gov.uk>
2019-06-28 17:49:57 +0100

    Renaming modules

--- securityhub.tf
+++ securityhub.tf
@@ -1,17 +1,17 @@
 # Activate Security Hub in Landing account
-module "unused-credentials-landing" {
+module "security-hub-landing" {
   source                     = "modules/securityhub"
   assume_role_in_account_id  = "${var.ap_accounts["landing"]}"
 }
 
 # Activate Security Hub in dev account
-module "unused-credentials-dev" {
+module "security-hub-dev" {
   source                     = "modules/securityhub"
   assume_role_in_account_id  = "${var.ap_accounts["dev"]}"
 }
 
 # Activate Security Hub in prod account
-module "unused-credentials-prod" {
+module "security-hub-prod" {
   source                     = "modules/securityhub"
   assume_role_in_account_id  = "${var.ap_accounts["prod"]}"
 }
