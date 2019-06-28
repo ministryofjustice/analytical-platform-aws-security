a357cd29faeef8d4896911d7a45040c4a2619628
Oliver Butterbach <oliver.butterbach@digital.justice.gov.uk>
2019-06-28 17:46:55 +0100

    Activate SecurityHub on dev and prod

--- securityhub.tf
+++ securityhub.tf
@@ -3,3 +3,15 @@ module "unused-credentials-landing" {
   source                     = "modules/securityhub"
   assume_role_in_account_id  = "${var.ap_accounts["landing"]}"
 }
+
+# Activate Security Hub in dev account
+module "unused-credentials-dev" {
+  source                     = "modules/securityhub"
+  assume_role_in_account_id  = "${var.ap_accounts["dev"]}"
+}
+
+# Activate Security Hub in prod account
+module "unused-credentials-prod" {
+  source                     = "modules/securityhub"
+  assume_role_in_account_id  = "${var.ap_accounts["prod"]}"
+}
