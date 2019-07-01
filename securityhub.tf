6c15b75990e0f6a65a5897d5515be5e99ac59028
Oliver Butterbach <oliver.butterbach@digital.justice.gov.uk>
2019-07-01 10:07:24 +0100

    Remove resource

--- securityhub.tf
+++ securityhub.tf
@@ -1,15 +1,15 @@
 # Activate Security Hub in Landing account
 module "unused-credentials-landing" {
-  # source                     = "modules/securityhub"
-  # assume_role_in_account_id  = "${var.ap_accounts["landing"]}"
+  source                     = "modules/securityhub"
+  assume_role_in_account_id  = "${var.ap_accounts["landing"]}"
+  count                      = 0
 }
 
 # Activate Security Hub in Landing account
-module "security-hub-landing" {
-  source                     = "modules/securityhub"
-  assume_role_in_account_id  = "${var.ap_accounts["landing"]}"
-}
-
+# module "security-hub-landing" {
+#   source                     = "modules/securityhub"
+#   assume_role_in_account_id  = "${var.ap_accounts["landing"]}"
+# }
 
 # Activate Security Hub in dev account
 module "security-hub-dev" {
