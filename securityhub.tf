fd7b1e21e175053e6c1e67d8dd7686a69fd73db9
Oli <4367158+obutterbach@users.noreply.github.com>
2019-07-01 10:18:41 +0100

    Remove module count (#86)

--- securityhub.tf
+++ securityhub.tf
@@ -2,7 +2,6 @@
 module "unused-credentials-landing" {
   source                     = "modules/securityhub"
   assume_role_in_account_id  = "${var.ap_accounts["landing"]}"
-  count                      = 0
 }
 
 # Activate Security Hub in Landing account
