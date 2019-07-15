01b6d5727021d209d04c4481a03e086ca2336486
Oli <4367158+obutterbach@users.noreply.github.com>
2019-07-15 15:39:46 +0100

    add order iam access s3 lambda (#105)

--- modules/lambda-s3-encryption/lambda.tf
+++ modules/lambda-s3-encryption/lambda.tf
@@ -177,6 +177,7 @@ resource "aws_lambda_permission" "allow_cloudwatch_to_call" {
     function_name = "${aws_lambda_function.lambda_s3_encryption.function_name}"
     principal = "events.amazonaws.com"
     source_arn = "${aws_cloudwatch_event_rule.schedule.arn}"
+    depends_on = ["aws_lambda_function.lambda_s3_encryption"]
 }
 
 # -----------------------------------------------------------
