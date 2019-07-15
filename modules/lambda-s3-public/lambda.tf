89bb9be56a4eac3c7bd118b57b2fc3859901f617
Oli <4367158+obutterbach@users.noreply.github.com>
2019-07-15 15:51:52 +0100

    Ob/remove blockers (#107)
    * remove blocking resources

--- modules/lambda-s3-public/lambda.tf
+++ modules/lambda-s3-public/lambda.tf
@@ -34,11 +34,10 @@ EOF
 # set up AWS Cloudwatch Event to target a lambda function
 # -----------------------------------------------------------
 
-resource "aws_cloudwatch_event_target" "main" {
-  rule       = "${aws_cloudwatch_event_rule.schedule.name}"
-  arn        = "${aws_lambda_function.lambda_s3_public.arn}"
-  depends_on = ["aws_lambda_function.lambda_s3_public"]
-}
+# resource "aws_cloudwatch_event_target" "main" {
+#   rule       = "${aws_cloudwatch_event_rule.schedule.name}"
+#   arn        = "${aws_lambda_function.lambda_s3_public.arn}"
+# }
 
 resource "aws_lambda_function" "lambda_s3_public" {
   filename         = "${var.filename}"
