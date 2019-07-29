1d3165809a300109a159b2f4109ccf4837320812
Oli <4367158+obutterbach@users.noreply.github.com>
2019-07-29 14:52:03 +0100

    Remove PUT option S3 IAM, dryrun (#139)

--- s3_public_access.tf
+++ s3_public_access.tf
@@ -15,3 +15,9 @@ module "prod-s3-public" {
   source                     = "modules/lambda-s3-public"
   assume_role_in_account_id  = "${var.ap_accounts["prod"]}"
 }
+
+# Scan for public / private s3 buckets in data
+module "data-s3-public" {
+  source                     = "modules/lambda-s3-public"
+  assume_role_in_account_id  = "${var.ap_accounts["data"]}"
+}
