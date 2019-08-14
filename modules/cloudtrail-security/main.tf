6e78e662b918bf590ecf8b710be12fcd8968b925
Oliver Butterbach <oliver.butterbach@digital.justice.gov.uk>
2019-08-14 12:37:34 +0100

    Send all global events

--- modules/cloudtrail-security/main.tf
+++ modules/cloudtrail-security/main.tf
@@ -6,5 +6,5 @@ resource "aws_cloudtrail" "foobar" {
   name                          = "tf-trail-foobar"
   s3_bucket_name                = "${var.s3_bucket_name}"
   s3_key_prefix                 = "${var.assume_role_in_account_id}"
-  include_global_service_events = false
+  include_global_service_events = true
 }
