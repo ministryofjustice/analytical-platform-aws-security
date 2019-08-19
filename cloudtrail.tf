81414cfd5e32f2b6f2093bd71a4d79b0100f57ce
Oliver Butterbach <oliver.butterbach@digital.justice.gov.uk>
2019-08-19 09:40:17 +0100

    Deploy cloudtrail in dev and prod environment

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -3,3 +3,15 @@ module "landing-cloudtrail-security" {
   source                    = "modules/cloudtrail-security"
   assume_role_in_account_id = "${var.ap_accounts["landing"]}"
 }
+
+# Add trail for dev cloudtrail
+module "dev-cloudtrail-security" {
+  source                    = "modules/cloudtrail-security"
+  assume_role_in_account_id = "${var.ap_accounts["dev"]}"
+}
+
+# Add trail for prod cloudtrail
+module "prod-cloudtrail-security" {
+  source                    = "modules/cloudtrail-security"
+  assume_role_in_account_id = "${var.ap_accounts["prod"]}"
+}
