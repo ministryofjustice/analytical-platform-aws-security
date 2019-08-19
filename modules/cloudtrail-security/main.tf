518933e947fedca553b44264a628499bbfea0c99
Oliver Butterbach <oliver.butterbach@digital.justice.gov.uk>
2019-08-19 09:27:16 +0100

    activate cloudtrail all region

--- modules/cloudtrail-security/main.tf
+++ modules/cloudtrail-security/main.tf
@@ -6,4 +6,5 @@ resource "aws_cloudtrail" "aws_security_engineering" {
   name                          = "aws-security-trail-${var.assume_role_in_account_id}"
   s3_bucket_name                = "${var.s3_bucket_name}"
   include_global_service_events = true
+  is_multi_region_trail         = true
 }
