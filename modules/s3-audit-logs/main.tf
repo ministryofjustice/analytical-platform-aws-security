f51a372b0e5f155ddd0234406e9e7c47919c5c55
Oliver Butterbach <oliver.butterbach@digital.justice.gov.uk>
2019-09-02 11:24:11 +0100

    Fix syntax on aws security repo

--- modules/s3-audit-logs/main.tf
+++ modules/s3-audit-logs/main.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket" "audit_bucket" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm     = "AES256"
+        sse_algorithm = "AES256"
       }
     }
   }
